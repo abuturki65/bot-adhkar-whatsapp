960d824cf993ab03899655de149f2ae64f48544d
Ryan almalki <36896768+adhkarbot@users.noreply.github.com>
2021-08-13 22:27:31 +0300

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -10,7 +10,7 @@ module.exports = broadcast = async (client) => {
       const date = new Date(); 
       const allid = await client.getAllChatIds() 
           
-       if ((date.getHours() === 01 && date.getMinutes() === 00) || (date.getHours() === 04 && date.getMinutes() === 00) || (date.getHours() === 07 && date.getMinutes() === 00) || (date.getHours() === 10 && date.getMinutes() === 00) || (date.getHours() === 13 && date.getMinutes() === 00) || (date.getHours() === 16 && date.getMinutes() === 00) || (date.getHours() === 19 && date.getMinutes() === 00) || (date.getHours() === 22 && date.getMinutes() === 00)){ 
+       if ((date.getHours() === 21 && date.getMinutes() === 00) || (date.getHours() === 00 && date.getMinutes() === 00) || (date.getHours() === 09 && date.getMinutes() === 00) || (date.getHours() === 12 && date.getMinutes() === 00) || (date.getHours() === 01 && date.getMinutes() === 00) || (date.getHours() === 04 && date.getMinutes() === 00) || (date.getHours() === 07 && date.getMinutes() === 00) || (date.getHours() === 10 && date.getMinutes() === 00) || (date.getHours() === 13 && date.getMinutes() === 00) || (date.getHours() === 16 && date.getMinutes() === 00) || (date.getHours() === 19 && date.getMinutes() === 00) || (date.getHours() === 22 && date.getMinutes() === 00)){ 
        
         for (lop of allid){
            let listadk = adk[Math.floor(Math.random() * adk.length)]
@@ -22,7 +22,7 @@ module.exports = broadcast = async (client) => {
             }       
          }
 
-      else if ((date.getHours() === 02 && date.getMinutes() === 00) || (date.getHours() === 05 && date.getMinutes() === 00) || (date.getHours() === 08 && date.getMinutes() === 00) || (date.getHours() === 11 && date.getMinutes() === 00) || (date.getHours() === 14 && date.getMinutes() === 00) || (date.getHours() === 17 && date.getMinutes() === 00) || (date.getHours() === 20 && date.getMinutes() === 00) || (date.getHours() === 23 && date.getMinutes() === 00)){ 
+      else if ((date.getHours() === 15 && date.getMinutes() === 00) || (date.getHours() === 18 && date.getMinutes() === 00) || (date.getHours() === 03 && date.getMinutes() === 00) || (date.getHours() === 06 && date.getMinutes() === 00) || (date.getHours() === 02 && date.getMinutes() === 00) || (date.getHours() === 05 && date.getMinutes() === 00) || (date.getHours() === 08 && date.getMinutes() === 00) || (date.getHours() === 11 && date.getMinutes() === 00) || (date.getHours() === 14 && date.getMinutes() === 00) || (date.getHours() === 17 && date.getMinutes() === 00) || (date.getHours() === 20 && date.getMinutes() === 00) || (date.getHours() === 23 && date.getMinutes() === 00)){ 
        
         for (lop of allid){
             let listphoto = photo[Math.floor(Math.random() * photo.length)]
@@ -34,17 +34,6 @@ module.exports = broadcast = async (client) => {
              }       
          }
 
-       else if ((date.getHours() === 03 && date.getMinutes() === 00) || (date.getHours() === 06 && date.getMinutes() === 00) || (date.getHours() === 09 && date.getMinutes() === 00) || (date.getHours() === 12 && date.getMinutes() === 00) || (date.getHours() === 15 && date.getMinutes() === 00) || (date.getHours() === 18 && date.getMinutes() === 00) || (date.getHours() === 21 && date.getMinutes() === 00) || (date.getHours() === 00 && date.getMinutes() === 00)){ 
-       
-        for (lop of allid){
-            let listvideo = videox[Math.floor(Math.random() * videox.length)]
-            let allchat = await client.getChatById(lop)
-           if (allchat.isGroup) await client.sendFileFromUrl(lop, listvideo, "video.mp4", "video").then((result) => { console.log('Result: ', result); })
-           .catch((error) => { console.error('Error when sending: ', error); });
-           
-    
-             }       
-         }
 
            
             } , 60000);
