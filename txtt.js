830ad9c8c6a49be7fbd4fff3e07650f453f49179
Ryan almalki <36896768+adhkarbot@users.noreply.github.com>
2021-08-13 22:18:54 +0300

    Update txtt.js

--- txtt.js
+++ txtt.js
@@ -4,6 +4,8 @@ module.exports = {
   t1:fs.readFileSync('./txtt/t1.txt',{encoding:'utf8', flag:'r'}),
   t2:fs.readFileSync('./txtt/t2.txt',{encoding:'utf8', flag:'r'}),
   t3:fs.readFileSync('./txtt/t3.txt',{encoding:'utf8', flag:'r'}),
+  t4:fs.readFileSync('./txtt/t4.txt',{encoding:'utf8', flag:'r'}),
+  t5:fs.readFileSync('./txtt/t5.txt',{encoding:'utf8', flag:'r'}),
 
   } 
    
